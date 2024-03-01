9a71f2876fcfc625d9941622f2700df24a1eac45
OpenAst <isrealme0@gmail.com>
2024-03-01 03:34:05 -0800

    database created

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -77,10 +77,13 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'mysql',
+        'USER': 'root',
+        'PASSWORD': 'Zert!7930',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
